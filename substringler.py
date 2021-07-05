be9ae170a292d4501860d9d83e8bb4defb8cf091
Engin İNSEL <76450122+enqinsel@users.noreply.github.com>
2021-07-05 21:51:45 +0300

    Substrings
    Alt dizeler

---
+++ substringler.py
@@ -0,0 +1,11 @@
+#SUBSTRINGLER
+    #Elimizdeki string ifadelerin alt kümesini almak, alt bileşenlerine erişmek demek.
+    #Seçim işlemi yaparken indekslerden yararlanıyoruz.
+    
+gel_yaz = "geleceği_yazanlar"
+
+gel_yaz[1] # e
+
+gel_yaz[0:3] #Sol taraf dahil edilir , sağ taraf dahil edilmez, ondan bir önceki indeksi seçer. Sonuç: gel
+
+gel_yaz[7:10] # Sonuç: i_y
