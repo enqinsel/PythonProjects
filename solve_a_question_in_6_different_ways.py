e09318e05e73198f4cd9b319efd3111978279510
Engin İNSEL <76450122+enqinsel@users.noreply.github.com>
2021-07-22 22:37:51 +0300

    Solve A Question In 6 Different Ways
    Bir soruyu 6 farklı yol ile çözmek

---
+++ solve_a_question_in_6_different_ways.py
@@ -0,0 +1,34 @@
+#Soru: Kullanıcıdan isim ve soyisim alarak, kaç farklı şekilde ekrana yazdırabiliriz ?
+
+name = input("İsminizi Giriniz: ")
+surname = input("Soyadınızı Giriniz: ")
+
+#print("Birinci Yöntem: " , name , surname)
+
+# =============================================================================
+# def customer():
+#     print("İkinci Yöntem: " , name , surname)
+# customer()
+# =============================================================================
+
+# print("Üçüncü Yöntem: " + "\nİsim: " + name + "\nSoyisim: " + surname)
+
+# print("Dördüncü Yöntem: " , name , surname , sep="\n")
+
+# print("Beşinci Yöntem: " , name , surname , sep = "\n" , end=" ")
+
+ 
+
+# =============================================================================
+# print("Altıncı Yöntem")  
+# if bool(name):
+#     print("İsminiz: "  , name)
+# if bool(surname):
+#     print("Soyadınız: " , surname)
+# else:
+#     print("İfade Giriniz.")
+# =============================================================================
+
+
+    
+    
